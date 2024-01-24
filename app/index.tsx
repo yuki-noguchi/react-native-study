b953dc977c68d00f7dcfe5c65a8358c1f3c77905
yuki-noguchi <y.n.10191@gmail.com>
2024-01-24 19:01:42 +0900

    tailwindお試し

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,21 @@
-import { Link } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>index Page</Text>
-      <Link href="/about">go to about</Link>
-      <StatusBar style="auto" />
+    <View className="flex flex-col h-screen">
+      <View className="bg-blue-500 sticky top-0 h-16" />
+      <View className="bg-white border-b-slate-300 border-b-2 h-16" />
+
+      <View className="flex-1 items-center justify-center bg-white">
+        <Button title="hoge"></Button>
+        <Text className="text-black">index Page</Text>
+        <Link href="/about" className="text-black underline">
+          go to about
+        </Link>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
