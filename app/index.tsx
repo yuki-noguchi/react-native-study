8c3a22774bf4785fdc871a0514c1ddd455f8a6bd
yuki-noguchi <y.n.10191@gmail.com>
2024-01-25 11:24:49 +0900

    emotionおためし

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import styled, { css,  } from '@emotion/native'
+
+const LinkText = styled.Text({
+  color: 'blue',
+  textDecorationStyle: 'solid',
+  textDecorationColor: 'blue',
+  textDecorationLine: 'underline',
+})
 
 export default function App() {
+  const {height, width} = useWindowDimensions();
   return (
-    <View style={styles.container}>
-      <Text>index Page</Text>
-      <Link href="/about">go to about</Link>
+    <View style={css({
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: width < 800 ? 'orange' : '#fff'
+    })} >
+      <Text style={{color: 'red'}}>index Page</Text>
+      <Link href="/about"><LinkText>go to about</LinkText></Link>
       <StatusBar style="auto" />
     </View>
   );
