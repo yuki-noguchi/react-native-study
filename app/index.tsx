627e620c01db1709ee5f526b102fc3a1fc183855
yuki-noguchi <y.n.10191@gmail.com>
2024-01-25 17:15:45 +0900

    フォーマット

--- app/index.tsx
+++ app/index.tsx
@@ -1,45 +1,53 @@
-import { gql, useQuery,ApolloClient, InMemoryCache, ApolloProvider, } from '@apollo/client';
-import { Link } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, ScrollView, StyleSheet, Text, View } from 'react-native';
-import { FetchAllTodosDocument } from '../__generated__/graphql';
-
+import {
+  gql,
+  useQuery, useSuspenseQuery
+} from "@apollo/client";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import {
+  FlatList, StyleSheet,
+  Text,
+  View
+} from "react-native";
+import { FetchAllTodosDocument } from "../__generated__/graphql";
 
 gql`
-query FetchAllTodos {
-  todos {
-    data {
-      id
-      title
+  query FetchAllTodos {
+    todos {
+      data {
+        id
+        title
+      }
     }
   }
-}
-`
-
+`;
 
 export default function App() {
-  const {data} = useQuery(FetchAllTodosDocument)
+  const { data } = useQuery(FetchAllTodosDocument);
+  const {} = useSuspenseQuery(FetchAllTodosDocument);
 
   return (
     <View style={styles.container}>
       <Text>index Page</Text>
       <Link href="/about">go to about</Link>
       <StatusBar style="auto" />
-      <ScrollView>
-        {data?.todos?.data?.map(todo =>
-        <View 
-        key={todo?.id}
-        style={{
-          borderTopColor: 'gray',
-          borderBottomColor: 'gray',
-          borderTopWidth: 1,
-          borderBottomWidth: 1,
-          paddingVertical: 4
-        }}>
-            <Text>{todo?.title}</Text>
-        </View>
-            )}
-      </ScrollView>
+      <FlatList
+        data={data?.todos?.data}
+        renderItem={({ item }) => (
+          <View
+            key={item?.id}
+            style={{
+              borderTopColor: "gray",
+              borderBottomColor: "gray",
+              borderTopWidth: 1,
+              borderBottomWidth: 1,
+              paddingVertical: 4,
+            }}
+          >
+            <Text>{item?.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -47,8 +55,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
